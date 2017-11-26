ea3e0e2e7688668fcb9f86c7e472b9ca817fc96d
rochars <rocha.rafaelsilva@gmail.com>
2017-11-26 17:21:14 -0200

    refactored toBitDepth() into toInt() and toFloat()

--- index.js
+++ index.js
@@ -7,18 +7,14 @@
  */
 
 /**
- * Max number of values for each bit depth.
+ * Max number of different values for each bit depth.
  * @enum {number}
  */
 const BitDepthMaxValues = {
-    2: 4,
-    4: 16,
     8: 256,
     16: 65536,
     24: 16777216,
-    32: 4294967296,
-    40: 1099511627776,
-    48: 281474976710656
+    32: 4294967296
 };
 
 /**
@@ -37,71 +33,108 @@ function toBitDepth(data, originalBitDepth, targetBitDepth) {
     validateBitDepths(originalBitDepth, targetBitDepth);
     let len = data.length;
 
-    // Get the max values for both original and target bit depth
-    let oldMaxValue =
-        parseInt((BitDepthMaxValues[parseInt(originalBitDepth, 10)]) / 2, 10);
-    let newMaxValue =
-        parseInt((BitDepthMaxValues[parseInt(targetBitDepth, 10)]) / 2, 10);
-
-    // needs dithering if the target bit depth
-    // is lower than the original bit depth
-    //if (parseInt(targetBitDepth, 10) < parseInt(originalBitDepth, 10)) {
-        // TODO: dithering
-    //}
-
-    for (let i=0; i<len; i++) {
-        
+    for (let i=0; i<len; i++) {        
         // 8-bit samples are unsigned;
         // They are signed here before conversion
         // (other bit depths are all signed)
-        if (originalBitDepth == "8") {
-            data[i] -= 128;
-        }
+        data[i] = sign8Bit(data[i], originalBitDepth);
 
         // If it is a float-to-float or int-to-float conversion then
         // the samples in the target bit depth will be normalized in the
         // -1.0 to 1.0 range; there is no need to multiply
         if (targetBitDepth == "32f" || targetBitDepth == "64") {
-            if (originalBitDepth != "32f" && originalBitDepth != "64") {
-                if (data[i] > 0) {
-                    data[i] = data[i] / (oldMaxValue - 1);
-                } else {
-                    data[i] = data[i] / oldMaxValue;
-                }
-            }
+            data[i] = toFloat(data[i], originalBitDepth);
 
         // If it is a float-to-int or int-to-int conversion then the
         // samples will be de-normalized according to the bit depth
         }else {
-            
-            // If the original samples are float, then they are already
-            // normalized between -1.0 and 1.0; All that is need is to
-            // multiply the sample values by the new bit depth max value
-            if (originalBitDepth == "32f" || originalBitDepth == "64" ) {
-                if (data[i] > 0) {
-                    data[i] = data[i] * (newMaxValue - 1);
-                } else {
-                    data[i] = data[i] * newMaxValue;
-                }
-
-            // If the original samples are integers, then they need to be
-            // divided by the maximum values of its original bit depth
-            // (to normalize them between -1.0 and .10) and then multiply
-            // them by the new bit depth max value
-            } else {
-                if (data[i] > 0) {
-                    data[i] = parseInt((data[i] / (oldMaxValue - 1)) * newMaxValue - 1, 10);
-                } else {
-                    data[i] = parseInt((data[i] / oldMaxValue) * newMaxValue, 10);
-                }
-            }
-            
-            // Make the samples unsigned if the target bit depth is "8"
-            if (targetBitDepth == "8") {
-                data[i] += 128;
-            }
+            data[i] = toInt(data[i], originalBitDepth, targetBitDepth);
         }  
     }
+}
+
+/**
+ * Sign unsigned 8-bit data.
+ * @param {number} sample The sample.
+ * @param {string} originalBitDepth The original bit depth of the data.
+ *      One of "8", "16", "24", "32", "32f", "64"
+ */
+function sign8Bit(sample, originalBitDepth) {
+    if (originalBitDepth == "8") {
+        sample -= 128;
+    }
+    return sample;
+}
+
+/**
+ * Unsign signed 8-bit data.
+ * @param {number} sample The sample.
+ * @param {string} targetBitDepth The target bit depth of the data.
+ *      One of "8", "16", "24", "32", "32f", "64"
+ */
+function unsign8Bit(sample, targetBitDepth) {
+    if (targetBitDepth == "8") {
+        sample += 128;
+    }
+    return sample;
+}
+
+/**
+ * Change the bit depth from int to float.
+ * The input array is modified in-place.
+ * @param {number} sample The sample.
+ * @param {string} originalBitDepth The original bit depth of the data.
+ *      One of "8", "16", "24", "32", "32f", "64"
+ */
+function toFloat(sample, originalBitDepth) {
+    let oldMaxValue = parseInt((BitDepthMaxValues[originalBitDepth]) / 2, 10);
+    if (originalBitDepth != "32f" && originalBitDepth != "64") {
+        if (sample > 0) {
+            sample = sample / (oldMaxValue - 1);
+        } else {
+            sample = sample / oldMaxValue;
+        }
+    }
+    return sample;
+}
+
+/**
+ * Change the bit depth of the data.
+ * The input array is modified in-place.
+ * @param {number} sample The sample.
+ * @param {string} originalBitDepth The original bit depth of the data.
+ *      One of "8", "16", "24", "32", "32f", "64"
+ * @param {string} targetBitDepth The new bit depth of the data.
+ *      One of "8", "16", "24", "32", "32f", "64"
+ */
+function toInt(sample, originalBitDepth, targetBitDepth) {
+    // If the original samples are float, then they are already
+    // normalized between -1.0 and 1.0; All that is need is to
+    // multiply the sample values by the new bit depth max value
+    let oldMaxValue = parseInt((BitDepthMaxValues[originalBitDepth]) / 2, 10);
+    let newMaxValue = parseInt((BitDepthMaxValues[targetBitDepth]) / 2, 10);
+    if (originalBitDepth == "32f" || originalBitDepth == "64" ) {
+        if (sample > 0) {
+            sample = sample * (newMaxValue - 1);
+        } else {
+            sample = sample * newMaxValue;
+        }
+
+    // If the original samples are integers, then they need to be
+    // divided by the maximum values of its original bit depth
+    // (to normalize them between -1.0 and .10) and then multiply
+    // them by the new bit depth max value
+    } else {
+        if (sample > 0) {
+            sample =
+                parseInt((sample / (oldMaxValue - 1)) * newMaxValue - 1, 10);
+        } else {
+            sample = parseInt((sample / oldMaxValue) * newMaxValue, 10);
+        }
+    }
+    
+    // Make the samples unsigned if the target bit depth is "8"
+    return unsign8Bit(sample, targetBitDepth);
 }
 
 /**
